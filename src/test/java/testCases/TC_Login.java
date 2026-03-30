d81c685bf548efe5d0245283538f7e388e662a03
Yogesh1819 <tejinkaryogesh124@gmail.com>
2026-03-30 10:46:37 +0100

    Added LocalRun.xml file

--- src/test/java/testCases/TC_Login.java
+++ src/test/java/testCases/TC_Login.java
@@ -1,5 +1,6 @@
 package testCases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import pageObjects.LoginPage;
@@ -8,11 +9,26 @@ import testBase.BaseClass;
 public class TC_Login extends BaseClass
 {
 	@Test
-   public void loginTestCase()
+   public void loginTestCase() throws InterruptedException
    {
 	   LoginPage lp = new LoginPage(driver);
-	   lp.setUsernameAndPassword(p.getProperty("Practitioner"), p.getProperty("Password"));
+	//   lp.setUsernameAndPassword(p.getProperty("Practitioner"), p.getProperty("Password"));
+	   lp.setUsernamePassword(p.getProperty("Practitioner"), p.getProperty("Password"));
 	   lp.clickSigninBtn();
+	   lp.lockMenuPanel();
+	   
+	   boolean verification=lp.verifyResidnetsBtn();
+	   if(verification==true)
+	   {
+		   System.out.println("Test case is pass..");
+		   Assert.assertTrue(true);
+	   }
+	   else
+	   {
+		   System.out.println("Test case fail..");
+		   Assert.assertTrue(false);
+	   }
+	   Thread.sleep(4000);
    }
 	
 }
